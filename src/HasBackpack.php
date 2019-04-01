de0db4f500420f47c0589a3587ca55e957af6347
Daniel S. Deboer <daniel.s.deboer@gmail.com>
2019-04-01 14:48:32 -0400

    Add public methods to interface

--- src/HasBackpack.php
+++ src/HasBackpack.php
@@ -6,4 +6,7 @@ interface HasBackpack
 {
     public static function getTypes(): Types;
     public function hydrate(array $items): void;
+    public function isTouched (string $key): bool;
+    public function getTouched (): array;
+    public function getAll (): array;
 }
